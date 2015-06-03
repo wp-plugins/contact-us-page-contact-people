35ba05387f7ddcecc10de33bacb2877e98248410
a3rev <a3rev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-03 09:57:22 +0000

    Created tag 1.2.6.
    git-svn-id: https://plugins.svn.wordpress.org/contact-us-page-contact-people/tags/1.2.6@1173516 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/people-contact-init.php
+++ admin/people-contact-init.php
@@ -3,18 +3,21 @@
  * Call this function when plugin is deactivated
  */
 function people_contact_install(){
-	update_option('a3rev_wp_people_contact_lite_version', '1.1.4.1');
-	update_option('a3rev_wp_people_contact_version', '1.1.4.1');
-	update_option('a3rev_wp_people_contact_ultimate_version', '1.0.5.1');
-	
-	$contact_us_page_id = People_Contact_Functions::create_page( esc_sql( 'contact-us-page' ), '', __('Contact Us Page', 'cup_cp'), '[people_contacts]' );
-	update_option('contact_us_page_id', $contact_us_page_id);
+	update_option('a3rev_wp_people_contact_lite_version', '1.2.6');
+	update_option('a3rev_wp_people_contact_version', '1.3.0');
+	update_option('a3rev_wp_people_contact_ultimate_version', '1.2.0');
+
+	$contact_us_page_id = People_Contact_Functions::create_page( esc_sql( 'contact-us-page' ), 'contact_us_page_id', __('Contact Us Page', 'cup_cp'), '[people_contacts]' );
 	People_Contact_Profile_Data::install_database();
-	
+
 	// Set Settings Default from Admin Init
 	global $people_contact_admin_init;
 	$people_contact_admin_init->set_default_settings();
-	
+
+	// Build sass
+	global $a3_people_contact_less;
+	$a3_people_contact_less->plugin_build_sass();
+
 	update_option('a3rev_wp_people_contact_just_installed', true);
 }
 
@@ -52,10 +55,10 @@ add_filter( 'plugin_row_meta', array('People_Contact_Hook_Filter', 'plugin_extra
 
 	global $people_contact_admin_init;
 	$people_contact_admin_init->init();
-	
+
 	// Add upgrade notice to Dashboard pages
 	add_filter( $people_contact_admin_init->plugin_name . '_plugin_extension', array( 'People_Contact_Functions', 'plugin_pro_notice' ) );
-		
+
 	$admin_pages = $people_contact_admin_init->admin_pages();
 	if ( is_array( $admin_pages ) && count( $admin_pages ) > 0 ) {
 		foreach ( $admin_pages as $admin_page ) {
@@ -63,53 +66,39 @@ add_filter( 'plugin_row_meta', array('People_Contact_Hook_Filter', 'plugin_extra
 			add_action( $people_contact_admin_init->plugin_name . '-' . $admin_page . '_tab_end', array( 'People_Contact_Functions', 'plugin_extension_end' ) );
 		}
 	}
-	
+
 	add_action('init', array('People_Contact_AddNew', 'profile_form_action') );
-	
+
 	add_action( 'admin_menu', array( 'People_Contact_Hook_Filter', 'register_admin_screen' ), 9 );
-	
-	add_action( 'get_header', array( 'People_Contact_Hook_Filter', 'frontend_header_scripts' ) );
-	
-	add_action( 'wp_footer', array( 'People_Contact_Hook_Filter', 'frontend_footer_scripts' ) );
-		
-	// Include style into header
-	add_action( 'get_header', array( 'People_Contact_Hook_Filter', 'add_style_header') );
-	
-	// Include google fonts into header
-	add_action( 'wp_head', array( 'People_Contact_Hook_Filter', 'add_google_fonts'), 10 );
-		
-	// Add Custom style on frontend
-	add_action( 'wp_head', array( 'People_Contact_Hook_Filter', 'include_customized_style'), 11);
-	
-	// Add script to fix for IE
-	add_action( 'wp_head', array( 'People_Contact_Hook_Filter', 'fix_window_console_ie') );
-	
+
+	add_action( 'wp_enqueue_scripts', array( 'People_Contact_Hook_Filter', 'frontend_scripts_register' ) );
+
 	add_filter( 'body_class', array( 'People_Contact_Hook_Filter', 'browser_body_class'), 10, 2 );
-	
+
 	//Ajax Sort Contact
 	add_action('wp_ajax_people_update_orders', array( 'People_Contact_Hook_Filter', 'people_update_orders') );
 	add_action('wp_ajax_nopriv_people_update_orders', array( 'People_Contact_Hook_Filter', 'people_update_orders') );
-				
+
 	$GLOBALS['people_contact'] = new People_Contact();
-	
+
 	$GLOBALS['people_contact_shortcode'] = new People_Contact_Shortcode();
-	
+
 	// Include script admin plugin
 	if ( in_array( basename ($_SERVER['PHP_SELF']), array('admin.php', 'edit.php') ) && isset( $_REQUEST['page'] ) && in_array( $_REQUEST['page'], array('people-contact-manager', 'people-contact', 'people-contact-settings', 'people-category-manager' ) ) ) {
 		add_action('admin_head', array('People_Contact_Hook_Filter', 'admin_header_script'));
 	}
-	
+
 	// Check upgrade functions
 add_action('plugins_loaded', 'a3_people_contact_lite_upgrade_plugin');
 function a3_people_contact_lite_upgrade_plugin () {
-	
+
 	// Upgrade to 1.0.3
 	if(version_compare(get_option('a3rev_wp_people_contact_version'), '1.0.3') === -1){
 		People_Contact_Profile_Data::install_database();
 		People_Contact_Functions::upgrade_to_1_0_3();
 		update_option('a3rev_wp_people_contact_version', '1.0.3');
 	}
-	
+
 	// Upgrade to 1.1.1
 	if(version_compare(get_option('a3rev_wp_people_contact_version'), '1.1.1') === -1){
 		$people_contact_grid_view_layout = get_option( 'people_contact_grid_view_layout', array() );
@@ -118,25 +107,33 @@ function a3_people_contact_lite_upgrade_plugin () {
 			'grid_view_col'			=> $people_contact_grid_view_layout['grid_view_col'],
 		);
 		update_option( 'people_contact_global_settings', $people_contact_global_settings );
-		
+
 		$people_contact_location_map_settings = get_option( 'people_contact_location_map_settings', array() );
 		$people_contact_location_map_settings['map_width_responsive'] = $people_contact_location_map_settings['map_width'];
 		$people_contact_location_map_settings['map_width_fixed'] = $people_contact_location_map_settings['map_width'];
 		update_option( 'people_contact_location_map_settings', $people_contact_location_map_settings );
-		
+
 		update_option('a3rev_wp_people_contact_version', '1.1.1');
 	}
-	
+
 	// Upgrade to 1.1.4 for Lite version
 	if(version_compare(get_option('a3rev_wp_people_contact_version'), '1.1.4') === -1){
 		update_option('a3rev_wp_people_contact_version', '1.1.4');
-		
+
 		People_Contact_Functions::upgrade_to_lite_1_1_4();
 	}
-	
-	update_option('a3rev_wp_people_contact_lite_version', '1.1.4.1');
-	update_option('a3rev_wp_people_contact_version', '1.1.4.1');
-	update_option('a3rev_wp_people_contact_ultimate_version', '1.0.5.1');
-	
+
+	// Upgrade to 1.2.0
+	if(version_compare(get_option('a3rev_wp_people_contact_lite_version'), '1.2.0') === -1){
+		// Build sass
+		global $a3_people_contact_less;
+		$a3_people_contact_less->plugin_build_sass();
+		update_option('a3rev_wp_people_contact_lite_version', '1.2.0');
+	}
+
+	update_option('a3rev_wp_people_contact_lite_version', '1.2.6');
+	update_option('a3rev_wp_people_contact_version', '1.3.0');
+	update_option('a3rev_wp_people_contact_ultimate_version', '1.2.0');
+
 }
 ?>
--- classes/class-people-contact.php
+++ classes/class-people-contact.php
@@ -13,11 +13,11 @@
 class People_Contact {
 	var $admin_page,$contact_manager;
 	public $template_url = PEOPLE_CONTACT_PATH;
-	
+
 	public function People_Contact () {
 		$this->init();
 	}
-	
+
 	public function init () {
 		//Add Ajax Send Email
 		add_action('wp_ajax_send_a_contact', array( $this, 'send_a_contact'));
@@ -26,24 +26,23 @@ class People_Contact {
 		add_action('wp_ajax_load_ajax_contact_form', array( $this, 'load_ajax_contact_form') );
 		add_action('wp_ajax_nopriv_load_ajax_contact_form', array( $this, 'load_ajax_contact_form') );
 	}
-	
+
 	public function send_a_contact(){
-		check_ajax_referer( 'send-a-contact', 'security' );
 		$profile_email = trim($_REQUEST['profile_email']);
 		$profile_name = trim($_REQUEST['profile_name']);
 		$c_name = trim($_REQUEST['c_name']);
 		$c_email = trim($_REQUEST['c_email']);
 		$c_phone = trim($_REQUEST['c_phone']);
 		$c_message = trim($_REQUEST['c_message']);
-		
+
 		$send_copy = $_REQUEST['send_copy'];
-				
+
 		if(trim($_REQUEST['c_subject']) != ''){
 			$subject = trim($_REQUEST['c_subject']). ' ' . __('from', 'cup_cp'). ' ' .get_bloginfo('name');
 		}else{
 			$subject = __('Contact from', 'cup_cp').' '.get_bloginfo('name');
 		}
-		
+
 		$profile_data = array(
 			'subject' 			=> $subject,
 			'to_email' 			=> $profile_email,
@@ -55,24 +54,23 @@ class People_Contact {
 			'message'			=> $c_message,
 		);
 		$email_result = People_Contact_Functions::contact_to_people( $profile_data, $send_copy );
-		
+
 		echo $email_result;
-		
+
 		die();
 	}
-	
+
 	public function load_ajax_contact_form() {
-		check_ajax_referer( 'ajax-popup-contact', 'security' );
 		global $people_email_inquiry_popup_form_style;
 		global $people_contact_grid_view_icon;
-		
+
 		$inquiry_contact_text_button = __('SEND', 'cup_cp');
-		
+
 		$inquiry_contact_button_class = '';
 		$inquiry_contact_form_class = '';
-		
+
 		$contact_id = $_REQUEST['contact_id'];
-		
+
 		$data = People_Contact_Profile_Data::get_row( $contact_id, '', 'ARRAY_A' );
 		?>
 		<div class="custom_contact_popup <?php echo $inquiry_contact_form_class; ?>">
@@ -92,19 +90,19 @@ class People_Contact {
         	<input type="hidden" value="<?php esc_attr_e( stripslashes( $data['c_email'] ) );?>" id="profile_email_<?php echo $contact_id; ?>" name="profile_email" />
         	<input type="hidden" value="<?php esc_attr_e( stripslashes(  $data['c_title']) );?> <?php esc_attr_e( stripslashes( $data['c_name'] ) );?>" id="profile_name_<?php echo $contact_id; ?>" name="profile_name" />
             <div class="people_email_inquiry_field">
-                <label class="people_email_inquiry_label" for="c_name_<?php echo $contact_id; ?>"><?php _e('Name', 'cup_cp'); ?> <span class="gfield_required">*</span></label> 
+                <label class="people_email_inquiry_label" for="c_name_<?php echo $contact_id; ?>"><?php _e('Name', 'cup_cp'); ?> <span class="gfield_required">*</span></label>
                 <input type="text" name="c_name" id="c_name_<?php echo $contact_id; ?>" value="" /></div>
             <div class="people_email_inquiry_field">
                 <label class="people_email_inquiry_label" for="c_email_<?php echo $contact_id; ?>"><?php _e('Email', 'cup_cp'); ?> <span class="gfield_required">*</span></label>
                 <input type="text" name="c_email" id="c_email_<?php echo $contact_id; ?>" value="" /></div>
             <div class="people_email_inquiry_field">
-                <label class="people_email_inquiry_label" for="c_phone_<?php echo $contact_id; ?>"><?php _e('Phone', 'cup_cp'); ?> <span class="gfield_required">*</span></label> 
+                <label class="people_email_inquiry_label" for="c_phone_<?php echo $contact_id; ?>"><?php _e('Phone', 'cup_cp'); ?> <span class="gfield_required">*</span></label>
                 <input type="text" name="c_phone" id="c_phone_<?php echo $contact_id; ?>" value="" /></div>
             <div class="people_email_inquiry_field">
-                <label class="people_email_inquiry_label" for="c_subject_<?php echo $contact_id; ?>"><?php _e('Subject', 'cup_cp'); ?> </label> 
+                <label class="people_email_inquiry_label" for="c_subject_<?php echo $contact_id; ?>"><?php _e('Subject', 'cup_cp'); ?> </label>
                 <input type="text" name="c_subject" id="c_subject_<?php echo $contact_id; ?>" value="" /></div>
             <div class="people_email_inquiry_field">
-                <label class="people_email_inquiry_label" for="c_message_<?php echo $contact_id; ?>"><?php _e('Message', 'cup_cp'); ?> <span class="gfield_required">*</span></label> 
+                <label class="people_email_inquiry_label" for="c_message_<?php echo $contact_id; ?>"><?php _e('Message', 'cup_cp'); ?> <span class="gfield_required">*</span></label>
                 <textarea rows="3" name="c_message" id="c_message_<?php echo $contact_id; ?>"></textarea></div>
             <div class="people_email_inquiry_field">
                 <a class="people_email_inquiry_form_button <?php echo $inquiry_contact_button_class; ?>" id="people_email_inquiry_bt_<?php echo $contact_id; ?>" contact_id="<?php echo $contact_id; ?>"><?php echo $inquiry_contact_text_button; ?></a>
@@ -116,52 +114,54 @@ class People_Contact {
         </div>
         </div>
 		<?php
-			
+
 		die();
 	}
-	
+
 	public function create_contact_maps( $contacts = array() ) {
 		global $people_email_inquiry_fancybox_popup_settings;
 		global $people_contact_global_settings, $people_contact_grid_view_layout, $people_contact_grid_view_style, $people_contact_location_map_settings, $people_contact_grid_view_icon;
 		$suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
-		
-		$contact_us_page_id = get_option('contact_us_page_id');
-		if( !is_page() || ($contact_us_page_id != get_the_ID()) ) return;
+
+		global $contact_people_page_id;
+		if( !is_page() || ($contact_people_page_id != get_the_ID()) ) return;
 		if( !is_array($contacts) || count ($contacts) <= 0 ) return;
-		
+
 		wp_enqueue_script( 'jquery' );
+		People_Contact_Hook_Filter::frontend_scripts_enqueue();
+
 		wp_enqueue_script('maps-googleapis','https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false' );
 		wp_enqueue_script( 'fancybox', PEOPLE_CONTACT_JS_URL . '/fancybox/fancybox'.$suffix.'.js', array(), false, true );
 		wp_enqueue_style( 'woocommerce_fancybox_styles', PEOPLE_CONTACT_JS_URL . '/fancybox/fancybox.css' );
-			
+
 		$ajax_popup_contact = wp_create_nonce("ajax-popup-contact");
-		
+
 		$unique_id = rand(100,10000);
-		
+
 		$profile_email_page_link = '#';
-		
+
 		$grid_view_col = $people_contact_global_settings['grid_view_col'];
-		
+
 		$show_map = ( $people_contact_location_map_settings['hide_maps_frontend'] != 1 ) ? 1 : 0 ;
-		
+
 		$phone_icon = PEOPLE_CONTACT_IMAGE_URL.'/p_icon_phone.png';
 		$fax_icon = PEOPLE_CONTACT_IMAGE_URL.'/p_icon_fax.png';
 		$mobile_icon = PEOPLE_CONTACT_IMAGE_URL.'/p_icon_mobile.png';
 		$email_icon = PEOPLE_CONTACT_IMAGE_URL.'/p_icon_email.png';
-		
+
 		$center_address = 'Australia';
 		if ( $people_contact_location_map_settings['center_address'] != '') {
 			$center_address = $people_contact_location_map_settings['center_address'];
 		}
-		
+
 		$url = 'http://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($center_address).'&sensor=false';
 		$geodata = file_get_contents($url);
 		$geodata = json_decode($geodata);
 		$center_lat = $geodata->results[0]->geometry->location->lat;
 		$center_lng = $geodata->results[0]->geometry->location->lng;
-			
+
 		$latlng_center = $center_lat.','.$center_lng;
-		
+
 		$map_type = $people_contact_location_map_settings['map_type'];
 			if($map_type == ''){
 				$map_type = 'ROADMAP';
@@ -170,7 +170,7 @@ class People_Contact {
 			if($zoom_level <= 0){
 				$zoom_level = 16;
 			}
-		
+
 		?>
 		<script type="text/javascript">
 		<?php
@@ -212,7 +212,7 @@ class People_Contact {
 							$geodata = file_get_contents($url);
 							$geodata = json_decode($geodata);
 							$value['c_latitude'] = $geodata->results[0]->geometry->location->lat;
-							$value['c_longitude'] = $geodata->results[0]->geometry->location->lng;	
+							$value['c_longitude'] = $geodata->results[0]->geometry->location->lng;
 						}
 		echo $notes."['".esc_attr( stripslashes( $value['c_name']))."',".$value['c_latitude'].",".$value['c_longitude'].",".$i.",'".esc_attr( stripslashes( $value['c_address']))."',".$value['id'].",'".$src."','".trim(esc_attr( stripslashes( $value['c_phone'])))."','".esc_attr( stripslashes( $value['c_title']))."','".trim(esc_attr( stripslashes( $value['c_fax'])))."','".trim(esc_attr( stripslashes( $value['c_mobile'])))."','".trim(esc_attr( stripslashes( $value['c_email'])))."']";
 						$notes = ',';
@@ -220,7 +220,7 @@ class People_Contact {
 				}
 				?>
 			];
-		
+
 			function setMarkers<?php echo $unique_id; ?>(map, markers) {
 				var infotext = '';
 				jQuery("div.people_item<?php echo $unique_id; ?>").each(function(i) {
@@ -231,7 +231,7 @@ class People_Contact {
 					if (sites[7] != '') infotext += '<p><span class="p_icon_phone"><img src="<?php echo $phone_icon;?>" style="width:auto;height:auto" /></span> '+sites[7]+'</p>';
 					if (sites[9] != '') infotext += '<p><span class="p_icon_fax"><img src="<?php echo $fax_icon;?>" style="width:auto;height:auto" /></span> '+sites[9]+'</p>';
 					if (sites[10] != '') infotext += '<p><span class="p_icon_mobile"><img src="<?php echo $mobile_icon;?>" style="width:auto;height:auto" /></span> '+sites[10]+'</p>';
-					
+
 					if (sites[11] != '') infotext += '<p><span class="p_icon_email"><img src="<?php echo $email_icon;?>" style="width:auto;height:auto" /></span> <a style="cursor:pointer" class="direct_email direct_email<?php echo $unique_id; ?> direct_email_map" target="_blank" profile-id="'+sites[5]+'" href="<?php echo $profile_email_page_link; ?>'+sites[5]+'"><?php _e('Click Here', 'cup_cp'); ?></a></p>';
 					infotext += '</div></div>';
 					var marker = new google.maps.Marker({
@@ -243,32 +243,32 @@ class People_Contact {
 						c_id: sites[5]/*,
 						icon :  "/images/market.png"*/
 					});
-					
+
 					if ( typeof(sites[1]) != 'undefined' && sites[1] != '' && typeof(sites[2]) != 'undefined' && sites[2] != '' ) {
 						current_object.find(".people-entry-item").mouseover(function(i){
 							infowindow.setContent(marker.html);
 							infowindow.open(map, marker);
 						});
-						
+
 						current_object.find(".people-entry-item").mouseout(function(i){
 							infowindow.close();
 						});
 					}
-					
+
 					if (sites[11] != '') {
 						google.maps.event.addListener(marker, "click", function () {
 						var c_id = this.c_id;
 							var ajax_url='<?php echo admin_url('admin-ajax.php', 'relative');?>'+'?action=load_ajax_contact_form&contact_id='+c_id+'&security=<?php echo $ajax_popup_contact;?>';
 							var popup_wide = 520;
 
-							if ( people_group_ei_getWidth<?php echo $unique_id; ?>()  <= 568 ) { 
-								popup_wide = '95%'; 
+							if ( people_group_ei_getWidth<?php echo $unique_id; ?>()  <= 568 ) {
+								popup_wide = '95%';
 							}
 							jQuery.fancybox({
 								href: ajax_url,
 								//content: ajax_url,
 								centerOnScroll : <?php echo $people_email_inquiry_fancybox_popup_settings['fancybox_center_on_scroll'];?>,
-								transitionIn : '<?php echo $people_email_inquiry_fancybox_popup_settings['fancybox_transition_in'];?>', 
+								transitionIn : '<?php echo $people_email_inquiry_fancybox_popup_settings['fancybox_transition_in'];?>',
 								transitionOut: '<?php echo $people_email_inquiry_fancybox_popup_settings['fancybox_transition_out'];?>',
 								easingIn: 'swing',
 								easingOut: 'swing',
@@ -290,7 +290,7 @@ class People_Contact {
 							return false;
 						})
 					}
-					
+
 					if ( typeof(sites[1]) != 'undefined' && sites[1] != '' && typeof(sites[2]) != 'undefined' && sites[2] != '' ) {
 						google.maps.event.addListener(marker, 'mouseout', function() {
 						   //infowindow.close();
@@ -303,21 +303,21 @@ class People_Contact {
 				});
 			}
 			<?php } ?>
-			
+
 				var ajax_url2<?php echo $unique_id; ?>='<?php echo admin_url('admin-ajax.php', 'relative');?>'+'?action=load_ajax_contact_form&security=<?php echo $ajax_popup_contact;?>&contact_id=';
 				jQuery(document).on("click", ".direct_email<?php echo $unique_id; ?>", function(){
 					var c_id2 = jQuery(this).attr("profile-id");
-					
+
 
 						var popup_wide2 = 520;
-						if ( people_group_ei_getWidth<?php echo $unique_id; ?>()  <= 568 ) { 
-							popup_wide2 = '95%'; 
+						if ( people_group_ei_getWidth<?php echo $unique_id; ?>()  <= 568 ) {
+							popup_wide2 = '95%';
 						}
 						jQuery.fancybox({
 								href: ajax_url2<?php echo $unique_id; ?>+c_id2,
 								//content: ajax_url,
 								centerOnScroll : <?php echo $people_email_inquiry_fancybox_popup_settings['fancybox_center_on_scroll'];?>,
-								transitionIn : '<?php echo $people_email_inquiry_fancybox_popup_settings['fancybox_transition_in'];?>', 
+								transitionIn : '<?php echo $people_email_inquiry_fancybox_popup_settings['fancybox_transition_in'];?>',
 								transitionOut: '<?php echo $people_email_inquiry_fancybox_popup_settings['fancybox_transition_out'];?>',
 								easingIn: 'swing',
 								easingOut: 'swing',
@@ -338,64 +338,70 @@ class People_Contact {
 						});
 						return false;
 				});
-			
+
 			var popupWindow<?php echo $unique_id; ?>=null;
 
 			function profile_popup<?php echo $unique_id; ?>(url){
 				window.open(url,"_blank");
 			}
-			
+
 			function profile_parent_disable<?php echo $unique_id; ?>() {
 				if(popupWindow<?php echo $unique_id; ?> && !popupWindow<?php echo $unique_id; ?>.closed)
 				popupWindow<?php echo $unique_id; ?>.focus();
 			}
-			
+
 			function people_group_ei_getWidth<?php echo $unique_id; ?>() {
 				xWidth = null;
 				if(window.screen != null)
 				  xWidth = window.screen.availWidth;
-			
+
 				if(window.innerWidth != null)
 				  xWidth = window.innerWidth;
-			
+
 				if(document.body != null)
 				  xWidth = document.body.clientWidth;
-			
+
 				return xWidth;
 			}
 		</script>
     	<?php
-		wp_enqueue_script( 'jquery-masonry', PEOPLE_CONTACT_JS_URL.'/masonry/jquery.masonry.min.js');
-		
-		wp_register_script( 'jquery_modernizr', PEOPLE_CONTACT_JS_URL.'/masonry/modernizr-transitions.js');
-    	wp_enqueue_script( 'jquery_modernizr' );
+		wp_enqueue_script( 'jquery-masonry' );
+
+		global $wp_version;
+		$cur_wp_version = preg_replace('/-.*$/', '', $wp_version);
 		?>
         <script type="text/javascript">
         jQuery(window).load(function(){
 			var grid_view_col = <?php echo $grid_view_col;?>;
-			var screen_width = jQuery('body').width(); 
+			var screen_width = jQuery('body').width();
 			if(screen_width <= 750 && screen_width >= 481 ){
 				grid_view_col = 2;
 			}
 			jQuery('.people_box_content<?php echo $unique_id; ?>').imagesLoaded(function(){
-				jQuery(this).masonry({
+				jQuery('.people_box_content<?php echo $unique_id; ?>').masonry({
 					itemSelector: '.people_item<?php echo $unique_id; ?>',
-					columnWidth: jQuery('.people_box_content<?php echo $unique_id; ?>').width()/grid_view_col,
-					isAnimated: !Modernizr.csstransitions
+					<?php if ( version_compare( $cur_wp_version, '3.9', '<' ) ) { ?>
+					columnWidth: jQuery('.people_box_content<?php echo $unique_id; ?>').width()/grid_view_col
+					<?php } else { ?>
+					columnWidth: '.people-grid-sizer'
+					<?php } ?>
 				});
 			});
 		});
 		jQuery(window).resize(function() {
 			var grid_view_col = <?php echo $grid_view_col;?>;
-			var screen_width = jQuery('body').width(); 
+			var screen_width = jQuery('body').width();
 			if(screen_width <= 750 && screen_width >= 481 ){
 				grid_view_col = 2;
 			}
 			jQuery('.people_box_content<?php echo $unique_id; ?>').imagesLoaded(function(){
-				jQuery(this).masonry({
+				jQuery('.people_box_content<?php echo $unique_id; ?>').masonry({
 					itemSelector: '.people_item<?php echo $unique_id; ?>',
-					columnWidth: jQuery('.people_box_content<?php echo $unique_id; ?>').width()/grid_view_col,
-					isAnimated: !Modernizr.csstransitions
+					<?php if ( version_compare( $cur_wp_version, '3.9', '<' ) ) { ?>
+					columnWidth: jQuery('.people_box_content<?php echo $unique_id; ?>').width()/grid_view_col
+					<?php } else { ?>
+					columnWidth: '.people-grid-sizer'
+					<?php } ?>
 				});
 			});
 		});
@@ -406,15 +412,15 @@ class People_Contact {
 			$html .= '<div style="clear:both"></div>';
 			$html .= '<div class="people-entry">';
 			$html .= '<div style="clear:both"></div>';
-			
+
 			if($people_contact_location_map_settings['map_height'] <= 0){
 				$map_height = '400';
 			}else{
 				$map_height = $people_contact_location_map_settings['map_height'];
 			}
-			
+
 			$map_width_type = $people_contact_location_map_settings['map_width_type'];
-			
+
 			if($map_width_type == 'px'){
 				$map_width_type = 'px';
 				if($people_contact_location_map_settings['map_width_fixed'] <= 0){
@@ -426,20 +432,20 @@ class People_Contact {
 				$map_width_type = '%';
 				$map_width = $people_contact_location_map_settings['map_width_responsive'];
 			}
-		
+
 			$html .= '<div id="map_canvas'.$unique_id.'" class="map_canvas_container" style="width: '.$map_width.$map_width_type.'; height: '.$map_height.'px;float:left;"></div>';
 			$html .= '<div style="clear:both;margin-bottom:0em;" class="custom_title"></div>';
 			$html .= '<div style="clear:both;height:15px;"></div>';
 			$html .= '</div>';
 		}
-		
+
 		$grid_view_team_title = trim($people_contact_global_settings['grid_view_team_title']);
-		
+
 		if ( $grid_view_team_title != '' ) {
 			$html .= '<div class="custom_box_title"><h1 class="p_title">'.$grid_view_team_title.'</h1></div>';
 		}
 		$html .= '<div style="clear:both;margin-bottom:1em;"></div>';
-		$html .= '<div class="people_box_content people_box_content'.$unique_id.' pcol'.$grid_view_col.'">';
+		$html .= '<div class="people_box_content people_box_content'.$unique_id.' pcol'.$grid_view_col.'"><div class="people-grid-sizer"></div>';
 		$i = 0;
 		if(is_array($contacts) && count($contacts) > 0 ){
 			$i++;
@@ -450,7 +456,7 @@ class People_Contact {
 				}else{
 					$src = PEOPLE_CONTACT_IMAGE_URL.'/no-avatar.png';
 				}
-						
+
 				$html .= '<div class="people_item people_item'.$unique_id.'">';
 				$html .= '<div class="people-entry-item">';
 				$html .= '<div style="clear:both;"></div>';
@@ -464,17 +470,17 @@ class People_Contact {
 				}
 				if ( trim($value['c_fax']) != '') {
 				$html .= '<p style="margin-bottom:5px;"><span class="p_icon_fax"><img src="'.$fax_icon.'" style="width:auto;height:auto" /></span> '. esc_attr( stripslashes( $value['c_fax'] ) ).'</p>';
-				} 
+				}
 				if ( trim($value['c_mobile']) != '') {
 				$html .= '<p style="margin-bottom:5px;"><span class="p_icon_mobile"><img src="'.$mobile_icon.'" style="width:auto;height:auto" /></span> '. esc_attr( stripslashes($value['c_mobile'] ) ).'</p>';
 				}
-				
+
 				if ( trim($value['c_email']) != '') {
-	
+
 					$html .= '<p style="margin-bottom:0px;"><span class="p_icon_email"><img src="'.$email_icon.'" style="width:auto;height:auto" /></span> <a style="cursor:pointer" class="direct_email direct_email'.$unique_id.'" profile-id="'.$value['id'].'" href="#'.$value['id'].'">'.__('Click Here', 'cup_cp').'</a></p>';
 				}
 				$html .= '</div>';
-				
+
 				$html .= '</div>';
 				$html .= '<div style="clear:both;"></div>';
 				$html .= '</div>';
